9c4dcd97407e3a75e3de08ca00c87e6e3a7d9399
Devendra Milmile <d.h.milmile1@gmail.com>
2024-11-20 12:50:48 +0530

    Update angular version to v18

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import {
     ChangeDetectionStrategy,
     Component,
@@ -28,7 +28,11 @@ import { User } from '../../types/paginated-user';
     imports: [
         CommonModule,
         NgxCustomCarouselModule,
-        HttpClientModule,
+        
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule,
         MatCheckboxModule,
         FormsModule,
         ReactiveFormsModule,
